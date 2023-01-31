07d478cffa1bdbd83b6fa7193da598fb56bd5146
lennon.rodrigues <lennon.rodrigues@tallos.com.br>
2023-01-31 20:23:12 -0300

    feat: Implementando rota e metodo para inserir novos usuários

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -9,6 +9,26 @@ class PessoaController {
       return res.status(500).json(error.message)
     }
   }
+
+  static async buscarPessoa(req, res) {
+    try {
+      const { id } = req.params;
+      const pessoa = await database.Pessoas.findOne({ where: { id: Number(id) } })
+      return res.status(200).json(pessoa);
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async criarPessoa(req, res) {
+    try {
+      const pessoa = req.body;
+      await database.Pessoas.create(pessoa)
+      return res.status(200).json({message: "Usuário criado com sucesso."});
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,7 @@ const PessoaController = require('../controllers/PessoaController.js')
 const router = Router();
 
 router.get('/pessoas', PessoaController.listarPessoas)
+router.get('/pessoas/:id', PessoaController.buscarPessoa)
+router.post('/pessoas', PessoaController.criarPessoa)
 
 module.exports = router;

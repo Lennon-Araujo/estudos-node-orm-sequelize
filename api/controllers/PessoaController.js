c2997330d3960a99d09940c960f2e26293b7be7a
lennon.rodrigues <lennon.rodrigues@tallos.com.br>
2023-02-07 18:45:51 -0300

    feat - incluindo routes e controllers para atualizarPessoa e removerPessoa

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -22,9 +22,53 @@ class PessoaController {
 
   static async criarPessoa(req, res) {
     try {
-      const pessoa = req.body;
-      await database.Pessoas.create(pessoa)
-      return res.status(200).json({message: "Usuário criado com sucesso."});
+      const novaPessoa = req.body;
+
+      // Valida se pessoa já existe
+      const pessoa = await database.Pessoas.findOne({ where: { email: novaPessoa.email } })
+
+      if (pessoa) {
+        return res.status(409).json({ message: "Por favor utilize outro e-mail." });
+      }
+
+      await database.Pessoas.create(novaPessoa)
+      return res.status(200).json({ message: "Usuário criado com sucesso." });
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async atualizarPessoa(req, res) {
+    try {
+      const { id } = req.params;
+      const novaPessoa = req.body;
+      // Valida se pessoa já existe
+      const pessoa = await database.Pessoas.findOne({ where: { id: Number(id) } })
+
+      if (pessoa) {
+        await database.Pessoas.update(novaPessoa, { where: { id: Number(id) } })
+        return res.status(200).json({ message: "Usuário atualizado com sucesso."});
+      } else {
+        return res.status(404).json({ message: "Usuário não encontrado." });
+      }
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async removerPessoa(req, res) {
+    try {
+      const { id } = req.params;
+
+      // Valida se pessoa existe
+      const pessoa = await database.Pessoas.findOne({ where: { id: Number(id) } })
+
+      if (pessoa) {
+        await database.Pessoas.destroy({ where: { id: Number(id) } })
+        return res.status(200).json({ message: "Usuário deletado com sucesso." });
+      } else {
+        return res.status(404).json({ message: "Usuário não encontrado." });
+      }
     } catch (error) {
       return res.status(500).json(error.message)
     }
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -6,5 +6,7 @@ const router = Router();
 router.get('/pessoas', PessoaController.listarPessoas)
 router.get('/pessoas/:id', PessoaController.buscarPessoa)
 router.post('/pessoas', PessoaController.criarPessoa)
+router.put('/pessoas/:id', PessoaController.atualizarPessoa)
+router.delete('/pessoas/:id', PessoaController.removerPessoa)
 
 module.exports = router;
